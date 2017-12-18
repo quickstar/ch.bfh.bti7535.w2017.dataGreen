be72d9d225e347b1ca3c15bb704f70b91fa88fbe
joris.baiutti <joris.baiutti@gmail.com>
2017-12-18 18:25:32 +0100

    Import Reviews

--- src/main/java/ch.bfh.bti7535.w2017/Runner.java
+++ src/main/java/ch.bfh.bti7535.w2017/Runner.java
@@ -1,6 +1,16 @@
-package ch.bfh.bti7535.w2017;
+package main.java.ch.bfh.bti7535.w2017;
+
+
+import ch.bfh.bti7535.w2017.io.ReviewFiles;
 
 public class Runner {
     public static void main(String[] args) {
+        ReviewFiles reviewFiles = new ReviewFiles();
+        String pos = "C:\\gitProjects\\ch.bfh.bti7535.w2017.dataGreen\\src\\main\\java\\resources\\reviws\\pos";
+        String neg = "C:\\gitProjects\\ch.bfh.bti7535.w2017.dataGreen\\src\\main\\java\\resources\\reviws\\neg";
+        reviewFiles.readFiles(pos,neg);
+
+
+
     }
 }
